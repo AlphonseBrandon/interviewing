57ea26dc6e3eef9800da6fd2c2d4bc8cbec3016f
AlphonseBrandon <alphonse.brand@gmail.com>
2024-11-11 15:18:46 +0100

    duplicate integer passed all test

---
+++ 01_dsa/array/Duplicate Interger/duplicate_integer.py
@@ -0,0 +1,30 @@
+"""
+Author: Alphonse Brandon
+Date Created: 11/11/2024
+"""
+
+def duplicate_integer(nums: list[int]) -> bool:
+    """
+    Check if an arrar contains duplicate integers.
+
+    Examples:
+        >>> input1 = [1, 2, 3, 3]
+        >>> input2 = [1, 2, 3, 4]
+        >>> duplicate_integer(input1)
+        True
+        >>> duplicate_integer(input2)
+        False
+    """
+
+    harshset = set()
+    for n in nums:
+        if n in harshset:
+            return True
+        harshset.add(n)
+    return False
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
